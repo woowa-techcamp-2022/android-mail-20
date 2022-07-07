66c2d28bd255a6a6ed36dbe17fe150882b4c6e7d
양윤재 <diddbswo04@dgsw.hs.kr>
2022-07-07 15:32:13 +0900

    [UPDATE] ViewBinding Setting

--- app/src/main/java/com/woowatech/android_mail_20/main/MainActivity.kt
+++ app/src/main/java/com/woowatech/android_mail_20/main/MainActivity.kt
@@ -2,11 +2,17 @@ package com.woowatech.android_mail_20.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.woowatech.android_mail_20.R
+import com.woowatech.android_mail_20.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val fragmentManager = supportFragmentManager
     }
 }

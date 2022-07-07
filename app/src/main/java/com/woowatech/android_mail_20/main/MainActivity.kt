d4061bc2dc129eb2ccb33453e406076a07770268
양윤재 <diddbswo04@dgsw.hs.kr>
2022-07-07 15:31:04 +0900

    [MOVE] MainActivity Directory '/' to '/main'

--- app/src/main/java/com/woowatech/android_mail_20/main/MainActivity.kt
+++ app/src/main/java/com/woowatech/android_mail_20/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.woowatech.android_mail_20
+package com.woowatech.android_mail_20.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.woowatech.android_mail_20.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
